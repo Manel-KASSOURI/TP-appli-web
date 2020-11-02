ca5c5b0336df964ac72f8a2e0529573e5c50bd68
Manel-KASSOURI <72606722+Manel-KASSOURI@users.noreply.github.com>
2020-11-02 09:28:38 +0100

    Add files via upload

---
+++ tp2/formulaire.js
@@ -0,0 +1,54 @@
+
+//fonction validation:
+
+function validation() {
+
+    // METHODE UNE:
+
+    var f = document.getElementById("fname").value;
+    var l = document.getElementById("lname").value;
+    var d = document.getElementById("date").value;
+    var a = document.getElementById("adresse").value;
+    var m = document.getElementById("mail").value;
+
+
+    //Verifier que ts les champs sont remplis et comporte au moins 5 caractères
+    if (  (f && l && d && a && m !="" ) && (f.length>4) && (f.length<=20) && (l.length>4) && (l.length<=20)  && (a.length>4) && (a.length<=50) && (m.length>4) && (m.length<=30) ) {
+        
+        document.getElementById("resultat").innerHTML = " Bienvenue " +  document.getElementById("lname").value ;
+        
+      
+    }else{
+
+        document.getElementById("error").innerHTML = "Veuillez renseigner tous les champs et ils doivent contenir au moins 5 caractères !";
+
+    } 
+
+
+        
+    //METHODE DEUX:
+
+    // acceder a tous les inputs du formulaire choisit a l'aide de id
+   /*var inputs = document.getElementById("formulaire").getElementsByTagName("input");
+ 
+	for (var i = 0; i < inputs.length; i++) {
+        //pour afficher dans la console
+        console.log(inputs[i]);
+        
+		if ( (inputs[i].value !="") && (inputs[i].length > 4) ){
+
+            document.getElementById("resultat").innerHTML = " Bienvenue " +  document.getElementById("lname").value ;
+			
+        }else{
+
+            document.getElementById("error").innerHTML = "Veuillez renseigner tous les champs !";
+
+
+        }
+	}*/
+
+
+
+
+
+}
