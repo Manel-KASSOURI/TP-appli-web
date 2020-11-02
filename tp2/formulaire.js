b328c4e891558d787c5adc431e780819fa1858c2
Manel-KASSOURI <72606722+Manel-KASSOURI@users.noreply.github.com>
2020-11-02 11:21:19 +0100

    Add files via upload

--- tp2/formulaire.js
+++ tp2/formulaire.js
@@ -14,16 +14,30 @@ function validation() {
 
     //Verifier que ts les champs sont remplis et comporte au moins 5 caractères
     if (  (f && l && d && a && m !="" ) && (f.length>4) && (f.length<=20) && (l.length>4) && (l.length<=20)  && (a.length>4) && (a.length<=50) && (m.length>4) && (m.length<=30) ) {
-        
+
+        //Masquer l'erreur:s
+        document.getElementById("error").classList.remove("display");
+
+        //Afficher le resultat:
+        document.getElementById("resultat").classList.add("display");
         document.getElementById("resultat").innerHTML = " Bienvenue " +  document.getElementById("lname").value ;
         
       
     }else{
 
+
+        
+       //Masquer le resultat quand on affiche l'erreur:
+        document.getElementById("resultat").classList.remove("display");
+
+        //Afficher l'erreur :
+        document.getElementById("error").classList.add("display");
         document.getElementById("error").innerHTML = "Veuillez renseigner tous les champs et ils doivent contenir au moins 5 caractères !";
+      
 
     } 
 
+  
 
         
     //METHODE DEUX:
